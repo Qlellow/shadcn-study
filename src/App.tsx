4d348ca06214f53bbf881c06c7c0a170c411d814
Qlellow <changyeon0702@gmail.com>
2026-01-09 15:49:31 +0900

    fix: 모바일에서 Toaster가 나오는 위치 변경 및 Toaster duration 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Toaster } from '@/components/ui/sonner';
 import { ThemeProvider } from '@/contexts/ThemeProvider';
 import AppRouter from '@/routes/AppRouter';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 function App() {
+  const isMobile = useIsMobile();
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="antialiased">
-        <Toaster position="top-center" />
+        <Toaster position={isMobile ? 'bottom-center' : 'top-center'} />
         <AppRouter />
       </div>
     </ThemeProvider>
